fde11f3bbcd17461df59732d75d69000e0794648
joshvernon <jvernon_483@hotmail.com>
2019-12-17 23:55:17 -0600

    Delete mix-generated test module

--- apps/kv_server/test/kv_server_test.exs
+++
@@ -1,9 +0,0 @@
-defmodule KVServerTest do
-  use ExUnit.Case
-  doctest KVServer
-
-  @tag skip: "placehlder from generated project"
-  test "greets the world" do
-    assert KVServer.hello() == :world
-  end
-end
