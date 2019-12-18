b4ab606acffdebec4e0e5eace44c5a9a19c54b10
joshvernon <jvernon_483@hotmail.com>
2019-12-17 19:41:19 -0600

    Keep the mix-generated test as a placeholder but skip it for now

--- apps/kv_server/test/kv_server_test.exs
+++ apps/kv_server/test/kv_server_test.exs
@@ -2,6 +2,7 @@ defmodule KVServerTest do
   use ExUnit.Case
   doctest KVServer
 
+  @tag skip: "placehlder from generated project"
   test "greets the world" do
     assert KVServer.hello() == :world
   end
